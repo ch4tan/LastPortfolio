6ea2c3aa559c0cfe55efa893d0aff6b6317ab5dc
ch4tan <absolument@protonmail.com>
2026-08-18 14:40:06 +0200

    no translate 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import ProfileMarquee from "../components/Marquee";
 
 const Home = () => {
     return (
-        <div className="flex flex-col gap-10 h-full w-full lg:pt-0 justify-end lg:justify-center items-center text-white text-2xl text-center">
+        <div className="flex flex-col gap-10 h-full w-full lg:pt-0 justify-end lg:justify-center items-center text-white text-2xl text-center notranslate">
             <TypingEffect text="Bienvenue sur mon portfolio !" />
             <div className="hidden lg:flex w-96 h-96 justify-end items-end bg-[url('/me4.PNG')] bg-no-repeat bg-cover" />
             <ProfileMarquee />
